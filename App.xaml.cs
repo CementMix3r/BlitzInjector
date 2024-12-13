a47c55dfdbe9635ab9f2de230147a853cbcaf1c6
naomi <116236871+CementMix3r@users.noreply.github.com>
2024-12-13 09:58:42 +0000

    added the dpi scaling src

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace BlitzLauncher {
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            DpiHelper.SetProcessDpiAwareness(DpiHelper.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+            base.OnStartup(e);
+        }
+    }
+    internal static class DpiHelper {
+        [DllImport("shcore.dll")]
+        public static extern int SetProcessDpiAwareness(int dpiAwareness);
+        public const int DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE = 2;
+    }
+}
